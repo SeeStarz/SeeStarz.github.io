bfa72f128d5466fb78a820f2c491480ec5464e30
SeeStarz <seestarz@tutamail.com>
2024-09-19 15:55:17 +0700

    Make button work

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-console.log("Hola");
-console.log(document.getElementsByClassName("jsonly"));
-const elems = document.getElementsByClassName("jsonly");
+const disabled = document.getElementsByClassName("jsonly");
 
-for (const elem of elems) {
+for (const elem of disabled) {
   elem.style.display = "block";
   elem.style.visibility = "visible";
 }
 
-console.log(document.querySelector("#Projects .Flex"));
-document.querySelector("#Projects .Flex").style["overflow"] = "hidden";
-console.log(document.querySelector("#Projects .Flex"));
+current_slide = 1;
+function changeSlide(n) {
+  const slides = document.getElementsByClassName("Slides");
+  for (const emogus of slides) {
+    emogus.style.display = "none";
+    emogus.style.visibility = "hidden";
+  }
+
+  let i = current_slide + n;
+  if (i < 0) {
+    i = slides.length - 1;
+  }
+  if (i >= slides.length) {
+    i = 0;
+  }
+  slides[i].style.display = "block";
+  slides[i].style.visibility = "visible";
+  current_slide = i;
+}
+document.getElementById("projectright").onclick = function () {
+  changeSlide(1);
+};
+
+document.getElementById("projectleft").onclick = function () {
+  changeSlide(-1);
+};
